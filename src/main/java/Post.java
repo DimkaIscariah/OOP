1b8d94ebbbb794f703835b16d79c769306f94199
Dmitry <once87@yandex.ru>
2023-04-22 21:23:03 +0300

    Update

---
+++ src/main/java/Post.java
@@ -0,0 +1,11 @@
+public class Post {
+    public String name;
+    public String surname;
+    public String patronimic;
+    public String passport;
+    public String phone;
+    public boolean subscription;
+    public FormDate birthday;
+
+
+}
